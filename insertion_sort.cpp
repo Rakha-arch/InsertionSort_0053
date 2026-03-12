80d2972950b5a825e70250e5a7303b11d9f43739
Rakha-arch <miftahuzahran@gmail.com>
2026-03-12 15:05:26 +0700

    input data

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -7,3 +7,17 @@ int n;
 int main() {
     return 0;
 }
+
+void input() {
+    while (true) {
+        cout << "Masukkan jumlah data pada array: ";
+        cin >> n;
+        if (n <= 20) break;
+        else cout << "\nArray tidak boleh lebih dari 20.\n";
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+// Di main, panggil input();
