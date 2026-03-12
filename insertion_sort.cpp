5645d1230491f723a989039d3140ddb484fc7afc
Rakha-arch <miftahuzahran@gmail.com>
2026-03-12 15:08:05 +0700

    implementasi insertion

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -21,3 +21,16 @@ void input() {
     }
 }
 // Di main, panggil input();
+
+void insertionsort() {
+    int temp, j;
+    for (int i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
